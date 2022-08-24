334769b00879b31b73893a4ae7f687bc750658dc
Cris-Omar <c-o.hernandez@hotmail.com>
2022-08-24 18:07:10 +0200

    @media konfig und weitere verbesserungen

---
+++ src/components/MenuItems.js
@@ -0,0 +1,17 @@
+export const menuItems = [
+    {
+        title: "test",
+        path:"/test",
+        cName:"test"
+    },
+    {
+        title: "test1",
+        path:"/test1",
+        cName:"test1"
+    },
+    {
+        title: "test2",
+        path:"/test2",
+        cName:"test2"
+    }
+]
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "../styles/NavBar.css"
 import logo from "../assets/LogoTransp.png"
 import {Link} from "react-router-dom"
 
 
-export default function navBar() {
+export default function NavBar() {
+
+  //Open & close menu
+  const [click, setClick] = useState(false)
+  const handleClick = () => setClick(!click)
+  
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(!open)
+
+  const [locOpen, setLocOpen] = useState(false)
+  const handleLocOpen = () => setLocOpen(!locOpen)
+
   return (
-    <div className='navbar'>
-        <div className='logo'>
-            <Link to="/"><img src={logo} alt="Logo"/></Link>
-        </div>
-
-        <div className='menu'>
-            <Link to="/">RestEssBar CH</Link>
-
-            <div className='nav'>
-              <Link to="/standorte">Standorte</Link>
-              <div className='navContent'>
-                <Link to="/">Baden Restessbar</Link>
-                <Link to="/">Save our Food Davos </Link>
-              </div>
-            </div>
-            <Link to="/restessbar-gruenden">RestEssBar Gründen</Link>
-            <Link to="/spenden">Spenden</Link>
-            <Link to="/foodwaste-netzwerk">Foodwaste-Netzwerk</Link>
-            <Link to="/kontakt">Kontakt</Link>
-        </div>
+  <div className='navbar'>
+    <div className='logo'>
+      <Link to="/"><img src={logo} alt="Logo"/></Link>
     </div>
+    {/*big screen Navigation Menu starts here */}
+    <div className='menu'>
+      <div className='menuItem' onMouseEnter={handleOpen} onMouseLeave={handleOpen}>
+        <Link to="/">RestEssBar CH <i class="fa-solid fa-angle-down"></i></Link>
+        {open && (
+          <div className='menuContent'>
+            <div className='childItem'><Link to="/">Über uns</Link></div>
+            <div className='childItem'><Link to="/">Medien</Link></div>
+            <div className='childItem'><Link to="/">Bildarchiv</Link></div>
+          </div>
+          )}
+          </div>
+            <div className='menuItem' onMouseEnter={handleLocOpen} onMouseLeave={handleLocOpen}>
+              <Link to="/standorte" >Standorte <i class="fa-solid fa-angle-down"></i></Link>
+              {locOpen && (
+                <div className='menuContent'>
+                  <div className='childItem'><Link to="/">RestEssBar Baden</Link></div>
+                  <div className='childItem'><Link to="/">Save our food Davos</Link></div>
+                  <div className='childItem'><Link to="/">RestEssBarr Ebikon</Link></div>
+                  <div className='childItem'><Link to="/">RestEssBarr Frauenfeld</Link></div>
+                  <div className='childItem'><Link to="/">RestEssBarr Grenchen</Link></div>
+                </div>
+              )}
+            </div>
+            
+            <div className="menuItem"><Link to="/restessbar-gruenden">RestEssBar Gründen</Link></div>
+            <div className='menuItem'><Link to="/spenden">Spenden</Link></div>
+            <div className='menuItem'><Link to="/foodwaste-netzwerk">Foodwaste-Netzwerk</Link></div>
+            <div className='menuItem'><Link to="/kontakt">Kontakt</Link></div>
+            {/*big screen Navigation Menu ends here */}
+        </div>
+                    {/*small screen Navigation Menu starts here */}
+                    <div className='menuIcon' onClick={handleClick}>
+              <i class={click ? "fa-solid fa-xmark" : "fa-solid fa-bars"}></i>
+            </div>
+            <div className='subMenu'>
+              <ul><Link to="/">RestEssBar CH <i class="fa-solid fa-angle-down"></i></Link></ul>
+              <ul><Link to="/standorte" >Standorte <i class="fa-solid fa-angle-down"></i></Link></ul>
+              <ul><Link to="/restessbar-gruenden">RestEssBar Gründen</Link></ul>
+              <ul><Link to="/foodwaste-netzwerk">Foodwaste-Netzwerk</Link></ul>
+              <ul><Link to="/kontakt">Kontakt</Link></ul>
+            </div>
+  </div>
   )
 }
